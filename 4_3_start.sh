22c072fe3f8bd28852cd712df5df788d70bf157e
rushitjasani <rushitjasani@gmail.com>
2018-09-14 13:49:43 +0530

    done.

--- 4_3_start.sh
+++ 4_3_start.sh
@@ -5,9 +5,12 @@ read -sp "Password: " password
 
 mkdir ~/openvpn &> /dev/null
 
-echo $uname > ~/openvpn/sec_data.txt
-echo $password >> ~/openvpn/sec_data.txt 
-
+echo $uname > ~/openvpn/.sec_data.txt
+echo $password >> ~/openvpn/.sec_data.txt
+if [[ -e ~/openvpn/ubuntu.ovpn ]]
+then
+	rm ~/openvpn/ubuntu.ovpn
+fi
 wget -q https://vpn.iiit.ac.in/secure/ubuntu.ovpn --http-user $uname --http-password $password -P ~/openvpn/
 
 command -v openvpn &> /dev/null
@@ -16,6 +19,4 @@ then
     sudo apt install openvpn
 fi
 
-sudo -b openvpn --config ~/openvpn/ubuntu.ovpn --auth-user-pass ~/openvpn/sec_data.txt &> /dev/null
-
-#rm ~/openvpn/sec_data.txt
+sudo -b openvpn --config ~/openvpn/ubuntu.ovpn --auth-user-pass ~/openvpn/.sec_data.txt &> /dev/null
